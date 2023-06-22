619c9ff8ad1836a8614b8780ba29ad1a00c4b636
Godwin Okpe <Okpegodwin18@yahoo.com>
2023-06-22 08:37:37 -0700

    Fixed minor error in main function

--- getline.c
+++ getline.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
-		fprintf(stderr, /"Usage: %s input_file\n", argv[0]);
+		fprintf(stderr, "Usage: %s input_file\n", argv[0]);
 		return (1);
 	}
 	input_file = argv[1];
